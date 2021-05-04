c5f534555f76dbb7d13c9f7ba828cb1d97e29273
daczarne <daniel.czarnievicz@gmail.com>
2021-05-05 00:51:58 +0200

    insert new row with new schema

---
+++ 06_operations_on_datasets_and_tables/insert_new_row_with_new_schema.sql
@@ -0,0 +1,2 @@
+/* This query inserts a new row into the table, using the new schema, all other rows will be filled with null values */
+INSERT INTO `bigquery-demo-285417.dataset1.names3` VALUES ("Crom", "M", 2455, "S1")
