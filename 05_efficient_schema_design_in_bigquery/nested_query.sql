46653f86c1ff6b805caf7788c9c6c1a52d556f85
daczarne <daniel.czarnievicz@gmail.com>
2021-05-04 22:37:46 +0200

    nested query

--- 05_efficient_schema_design_in_bigquery/nested_query.sql
+++ 05_efficient_schema_design_in_bigquery/nested_query.sql
@@ -1,6 +1,6 @@
 SELECT 
   storeLocation
   , products
-FROM `bigquery-demo-285417.dataset1.kiopl`,
-UNNEST(products) AS a
+FROM `bigquery-demo-285417.dataset1.kiopl`
+CROSS JOIN UNNEST(products) AS a
 WHERE a.productName = "Grinder"
