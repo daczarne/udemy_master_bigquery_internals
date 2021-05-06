f6da4f60a3aa2fa52b1e5fa9cf63348ae712f43b
daczarne <daniel.czarnievicz@gmail.com>
2021-05-06 16:52:22 +0200

    creating materialized views

---
+++ 12_materialized_views_in_bigquery/01_creating_materialized_views.sql
@@ -0,0 +1,9 @@
+/* Create a materialized view */
+
+CREATE MATERIALIZED VIEW bigquery-demo-285417.dataset1.names_mv
+AS
+SELECT 
+  name
+  , SUM(count) AS total_count
+FROM bigquery-demo-285417.dataset1.names
+GROUP BY name
