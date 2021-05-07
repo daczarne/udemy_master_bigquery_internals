f8cf4a008b52ad1388ba55d604571d91a51b503f
daczarne <daniel.czarnievicz@gmail.com>
2021-05-07 16:43:05 +0200

    dry run

---
+++ 13_bq_cli/10_dry_run.sh
@@ -0,0 +1,16 @@
+# * For the query to just be validated but not run, include the --dry_run flag
+
+bq query \
+--use_legacy_sql=false \
+--label <key:value> \
+--label <key:value> \
+--batch=false \
+--maximum_bytes_billed=30000000 \
+--require_cache=false \
+--destination_table=<project_name>:<dataset_name>.<destination_table_name> \
+--destination_schema <col_name:col_type>,<col_name:col_type>,<col_name>:<col_type> \
+--time_partitioning_type=DAY \
+--time_partitioning_expiration=90000 \
+--clustering_fields=<col_name> \
+--dry_run
+"SELECT * FROM `project.dataset.source_table`"
