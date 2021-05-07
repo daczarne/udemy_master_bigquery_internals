4966fa9b0ba315ff666460191d35ce0c572dd6ce
daczarne <daniel.czarnievicz@gmail.com>
2021-05-07 19:16:40 +0200

    create datasets

---
+++ 13_bq_cli/11_create_dataset.sh
@@ -0,0 +1,7 @@
+# * Create a dataset
+
+bq mk \
+--default_table_expiration 4000 \
+--default_partition_expiration 5000 \
+--description "Dataset description goes here" \
+[<project_name>:]<dataset_name>
