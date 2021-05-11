ff795e624b22946afcba2d587e7eb93059d8f27a
daczarne <daniel.czarnievicz@gmail.com>
2021-05-11 18:26:37 +0200

    schema relaxation

--- 13_bq_cli/14_schema_relaxation.sh
+++ 13_bq_cli/14_schema_relaxation.sh
@@ -2,4 +2,4 @@
 
 bq load \
 --schema_update_option ALLOW_FIELD_RELAXATION \
-<project_name>:<dataset_name>.<table_name> <path_to_data_file> <schema>
+<project_name>:<dataset_name>.<table_name> <path_to_data_file> <path_to_schema_file.json>
