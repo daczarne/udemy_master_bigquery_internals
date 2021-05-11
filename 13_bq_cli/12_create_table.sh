d76d5e0d2bb44bc2c8c73eb22864099ad8bd5df5
daczarne <daniel.czarnievicz@gmail.com>
2021-05-11 17:04:25 +0200

    create tables

---
+++ 13_bq_cli/12_create_table.sh
@@ -0,0 +1,13 @@
+# * Create a table
+
+bq mk
+--table
+--expiration 3000
+--description "Table description goes here"
+--label dummy_key1:value1
+--label dummy_key2:value2
+--require_partition_filter
+--time_partitioning_type DAY
+--time_partitioning_expiration 4000
+--clustering_fields name
+--schema "<path_to_json_file> [<project_name>:]<dataset_name>.<table_name>"
