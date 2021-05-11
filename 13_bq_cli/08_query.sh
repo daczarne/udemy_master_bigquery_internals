2625677b38762502a430486b64600b52f439357d
daczarne <daniel.czarnievicz@gmail.com>
2021-05-11 18:22:57 +0200

    query

--- 13_bq_cli/08_query.sh
+++ 13_bq_cli/08_query.sh
@@ -1,3 +1,3 @@
-# * Use the query command and a SQL statement to run a query from the command line
+#* Use the query command and a SQL statement to run a query from the command line
 
 bq query --use_standard_sql=false <query_statement>
