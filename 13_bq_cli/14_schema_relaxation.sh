d7640b934d8a9f1cdf584e24e569c5564b522386
daczarne <daniel.czarnievicz@gmail.com>
2021-05-11 18:25:24 +0200

    schema relaxation

---
+++ 13_bq_cli/14_schema_relaxation.sh
@@ -0,0 +1,5 @@
+#* Table schema can be relaxed during a load or query command. 
+
+bq load \
+--schema_update_option ALLOW_FIELD_RELAXATION \
+<project_name>:<dataset_name>.<table_name> <path_to_data_file> <schema>
