420233a1d023d7d927844fbae1d91ff6ea88e9fe
daczarne <daniel.czarnievicz@gmail.com>
2021-05-11 18:25:15 +0200

    load data

--- 13_bq_cli/13_load_data.sh
+++ 13_bq_cli/13_load_data.sh
@@ -1,3 +1,3 @@
-# * Load data into table. If the table does not exist, it will be created.
+#* Load data into table. If the table does not exist, it will be created.
 
 bq load [<project_name>:]<dataset_name>.<table_name> <path_to_data_file> --schema <path_to_schema_file.json>
