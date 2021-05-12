46b096028c38a15b2b0909ff819dcc4e13580c9a
daczarne <daniel.czarnievicz@gmail.com>
2021-05-12 18:50:46 +0200

    create data set

--- 14_python_client_library/01_create_dateset.py
+++ 14_python_client_library/01_create_dateset.py
@@ -1,19 +1,20 @@
 from google.cloud import bigquery
 
+#* Set service account key
 SERVICE_ACCOUNT_JSON = "path_to_key.json"
 
 #* Construct a BigQuery client object.
 client = bigquery.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
 
-# TODO(developer): Set dataset_id to the ID of the dataset to create.
+#* Set dataset_id to the ID of the dataset to create.
 dataset_id = "project_name.dataset_name"
 
-#* Construct a full Dataset object to send to the API.
+#* Construct a full Dataset object to send to the API using the Dataset class.
 dataset = bigquery.Dataset(dataset_id)
 
-# TODO(developer): Specify the geographic location where the dataset should reside.
+#* Specify the geographic location where the dataset should reside. Other dataset attributes can be found in the documentation: https://googleapis.dev/python/bigquery/latest/generated/google.cloud.bigquery.dataset.Dataset.html
 dataset.location = "US"
-dataset.description = "my new dataset"
+dataset.description = "dataset description"
 
 #* Send the dataset to the API for creation, with an explicit timeout. Raises google.api_core.exceptions.Conflict if the Dataset already exists within the project.
 dataset_ref = client.create_dataset(dataset, timeout = 30)  # Make an API request.
