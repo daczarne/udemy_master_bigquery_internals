6fa66b55ce2e710d8b0c6ad5297664f2930d5679
daczarne <daniel.czarnievicz@gmail.com>
2021-05-13 19:45:48 +0200

    data pipeline

--- 15_build_end_to_end_data_pipelines/data_pipeline.py
+++ 15_build_end_to_end_data_pipelines/data_pipeline.py
@@ -1,11 +1,68 @@
-#* Names of the tables to which data is going to be uploaded
-delivered_table_spec = '<project_name>:<dataset_name>.delivered_orders'
-other_table_spec = '<project_name>:<dataset_name>.other_status_orders'
+#! Import libraries and modules
 
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
 import argparse
 from google.cloud import bigquery
+from apache_beam.runners.runner import PipelineState
+
+
+#! Table specs
+#* Names of the tables to which data is going to be uploaded
+delivered_table_spec = '<project_name>:<dataset_name>.delivered_orders'
+other_table_spec = '<project_name>:<dataset_name>.other_status_orders'
+
+
+#! Use case specific transformations. These functions will clean the data.
+
+#* This function removes the colon at the end of the row
+def remove_last_colon(row):
+	cols = row.split(',')
+	item = str(cols[4])
+	if item.endswith(':'):
+		cols[4] = item[:-1]
+	return ','.join(cols)
+
+
+#* This function removes all special characters. It replaces them for an empty string
+def remove_special_characters(row):
+	import re
+	cols = row.split(',')
+	ret = ''
+	for col in cols:
+		clean_col = re.sub(r'[?%&]', '', col)
+		ret = ret + clean_col + ','
+		ret = ret[:-1]
+	return ret
+
+#* Print rows
+def print_row(row):
+	print(row)
+
+
+#* Transform data to JSON format
+def to_json(csv_str):
+	fields = csv_str.split(',')
+	json_str = {
+		"customer_id":fields[0],
+		"date": fields[1],
+		"timestamp": fields[2],
+		"order_id": fields[3],
+		"items": fields[4],
+		"amount": fields[5],
+		"mode": fields[6],
+		"restaurant": fields[7],
+		"status": fields[8],
+		"ratings": fields[9],
+		"feedback": fields[10],
+		"new_col": fields[11]
+	}
+	return json_str
+
+
+#! #################### !#
+#! Apache Beam pipeline !#
+
 
 #* Create an argument parser object
 parser = argparse.ArgumentParser()
@@ -28,33 +85,6 @@ options = PipelineOptions(pipeline_args)
 
 #* Instance of pipeline class
 p = beam.Pipeline(options = options)
-
-
-#* Use case specific transformations. These functions will clean the data.
-
-#* This function removes the colon at the end of the row
-def remove_last_colon(row):
-	cols = row.split(',')
-	item = str(cols[4])
-	if item.endswith(':'):
-		cols[4] = item[:-1]
-	return ','.join(cols)
-
-
-#* This function removes all special characters. It replaces them for an empty string
-def remove_special_characters(row):
-	import re
-	cols = row.split(',')
-	ret = ''
-	for col in cols:
-		clean_col = re.sub(r'[?%&]', '', col)
-		ret = ret + clean_col + ','
-		ret = ret[:-1]
-	return ret
-
-
-def print_row(row):
-	print(row)
 
 
 #* Create a collection that uses the pipeline options and runs the cleaning transformations on it. We start with the p object and then use the | (pipeline operator) to apply transformations to the data
@@ -82,7 +112,7 @@ delivered_orders = (
 #* Other orders collection (status not delivered)
 other_orders = (
 	cleaned_data
-  | 'Undelivered Filter' >> beam.Filter(lambda row: row.split(',')[8].lower() != 'delivered')
+  | 'undelivered filter' >> beam.Filter(lambda row: row.split(',')[8].lower() != 'delivered')
 )
 
 #* This is just of testing purposed. Take the counts and compare them.
@@ -117,36 +147,22 @@ other_orders = (
 #* Create a BigQuery client instance. 
 client = bigquery.Client()
 
-dataset_id = "{}.dataset_food_orders".format(client.project)
+#* Create the dataset
+dataset_id = "{}.<dataset_name>".format(client.project)
 
 try:
 	client.get_dataset(dataset_id)
 except:
-	dataset = bigquery.Dataset(dataset_id)  #
+	dataset = bigquery.Dataset(dataset_id)
 	dataset.location = "US"
 	dataset.description = "dataset for food orders"
-	dataset_ref = client.create_dataset(dataset, timeout=30)  # Make an API request.
+	dataset_ref = client.create_dataset(dataset, timeout=30)
 
-def to_json(csv_str):
-	fields = csv_str.split(',')
-	json_str = {
-		"customer_id":fields[0],
-		"date": fields[1],
-		"timestamp": fields[2],
-		"order_id": fields[3],
-		"items": fields[4],
-		"amount": fields[5],
-		"mode": fields[6],
-		"restaurant": fields[7],
-		"status": fields[8],
-		"ratings": fields[9],
-		"feedback": fields[10],
-		"new_col": fields[11]
-	}
-	return json_str
 
+#* Define table schema (needs to be the same as the JSON)
 table_schema = 'customer_id:STRING,date:STRING,timestamp:STRING,order_id:STRING,items:STRING,amount:STRING,mode:STRING,restaurant:STRING,status:STRING,ratings:STRING,feedback:STRING,new_col:STRING'
 
+#* Use the WriteToBigQuery method to load the data con delivered orders
 (
 	delivered_orders
 	| 'delivered to json' >> beam.Map(to_json)
@@ -159,6 +175,7 @@ table_schema = 'customer_id:STRING,date:STRING,timestamp:STRING,order_id:STRING,
 	)
 )
 
+#* Use the WriteToBigQuery method to load the data con other orders
 (
 	other_orders
 	| 'others to json' >> beam.Map(to_json)
@@ -167,28 +184,41 @@ table_schema = 'customer_id:STRING,date:STRING,timestamp:STRING,order_id:STRING,
 		schema = table_schema,
 		create_disposition = beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
 		write_disposition = beam.io.BigQueryDisposition.WRITE_APPEND,
-		additional_bq_parameters = {'timePartitioning': {'type': 'DAY'}}
+		additional_bq_parameters = {
+			'timePartitioning': {'type': 'DAY'}
+		}
 	)
 )
 
-from apache_beam.runners.runner import PipelineState
-
+#* Run
 ret = p.run()
 
+#* Print the status of the operation
 if ret.state == PipelineState.DONE:
 	print('Success!!!')
 else:
 	print('Error Running beam pipeline')
 
 
-view_name = "daily_food_orders"
-dataset_ref = client.dataset('dataset_food_orders')
+#! ################################# !#
+#! Create a view of delivered orders !#
+
+#* View definitions
+view_name = "<view_name>"
+dataset_ref = client.dataset('<dataset_name_where_the_view_will_be_created>')
 view_ref = dataset_ref.table(view_name)
 view_to_create = bigquery.Table(view_ref)
 
-view_to_create.view_query = 'select * from `bigquery-demo-285417.dataset_food_orders.delivered_orders` where _PARTITIONDATE = DATE(current_date())'
+#* View creation query
+view_to_create.view_query = """
+	SELECT *
+	FROM `<project_name>.<dataset_name>.<source_table_name>`
+	WHERE _PARTITIONDATE = DATE(CURRENT_DATE())
+"""
+
 view_to_create.view_use_legacy_sql = False
 
+#* Use Try-Catch so that it does not fail on subsequent runs
 try:
 	client.create_table(view_to_create)
 except:
