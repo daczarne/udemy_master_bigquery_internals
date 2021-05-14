554856ed627f872013c9b612790246dbfe18e5ea
daczarne <daniel.czarnievicz@gmail.com>
2021-05-14 12:04:46 +0200

    message simulation

---
+++ 16_build_streaming_data_pipelines/01_message_simulation.py
@@ -0,0 +1,23 @@
+from google.cloud import pubsub_v1
+import time
+
+#* Instantiate a publisher client object
+publisher = pubsub_v1.PublisherClient()
+
+#* Name of the Topic
+topic_name = 'projects/bigquery-demo-285417/topics/data_stream_from_file'
+
+#* Create the topic
+try:
+	publisher.create_topic(topic_name)
+except:
+	print('Topic already exists')
+
+#* Simulate message publishing
+with open('food_daily.csv') as f_in:
+	for line in f_in:
+		#* Data must be a bytestring
+		data = line
+		future = publisher.publish(topic_name, data = data)
+		print(future.result())
+		time.sleep(1)
